783ab8f595d34da191034057f1bf3ca510ebefcb
Vinay Kumar Kotegowder <VinayKumar.Kotegowder@arm.com>
2018-07-28 16:45:29 +0530

    Adding example program of ANN

---
+++ python/02_Deep_Learning_A_Z/ann.py
@@ -0,0 +1,36 @@
+# Artificial Neural Network
+
+# Theano
+"""
+Open source Numerical computation library - very efficient for fast
+numerical computations and is based on Numpy syntax.
+
+"""
+# Tensorflow
+"""
+Open souce Numerical computation library - Under Apache License
+
+"""
+# Keras
+"""
+Library based on Theano and Tensorflow used to build
+Deep Neural Netowrk with few lines of code
+
+"""
+# Installing Theano, TensorFlow and Keras
+"""
+1. Go to Tools -> Open command prompt
+2. Create a new environment with Anaconda and Python 3.5:
+    conda create -n tensorflow python=3.5 anaconda
+3. Activate the environment:
+    activate tensorflow
+4. Install Theano, TensorFlow and Keras:
+    conda install theano
+    conda install mingw libpython
+    pip install tensorflow
+    pip install keras
+5. Update the packages
+    conda update --all
+6. spyder
+
+"""
